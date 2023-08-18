ba84a71a6f242681d7c3eae7cc2a199eeabfe1c3
JAckLR64y4647 <106735054+JAckLR64y4647@users.noreply.github.com>
2023-08-18 21:18:34 +0300

    update

--- urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-     path('books/', views.book_list, name='book_list'),
-    path('reserve/<int:book_id>/', views.reserve_book, name='reserve_book'),
-]
